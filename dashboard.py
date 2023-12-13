a6ddc5131460ed74235b7bed1797b9232d3247b4
archishmagoli <agoli6@gatech.edu>
2023-12-13 10:58:26 -0500

    add data file for testing

--- dashboard.py
+++ dashboard.py
@@ -10,7 +10,7 @@ import plotly.graph_objects as go
 from datetime import datetime
 
 # Load the dataframe
-sm_df = pd.read_pickle('sentiment_data_vader_labels.pkl')
+sm_df = pd.read_pickle('peach2-analytics-dashboard\sentiment_data_vader_labels.pkl')
 
 # authoredAt column manipulation for timeseries grouping
 sm_df['authoredAt'] = pd.to_datetime(sm_df['authoredAt'])
@@ -189,29 +189,43 @@ maindiv = html.Div(
     id="first-div",
     children=[
        html.H1('Social Media Analytics Dashboard'),
+       html.Div([dcc.Graph(
+                        id='sentiment-pie-chart',
+                        figure=px.pie(
+                            sentiment_count,
+                            names='sentiment',
+                            values='count',
+                            title='Post Sentiment Breakdown',
+                            labels={'sentiment': 'Sentiment', 'count': 'Count'},
+                            color='sentiment',
+                            color_discrete_map={'positive': 'green', 'negative': 'red', 'neutral': 'blue'}
+                        ),
+                        style={'border': '2px solid #e8e8e8', 'background-color': '#f8f9fa'}
+                ),
+                dbc.Tooltip(
+                    "My Cooler Hover Tooltip",
+                    target="sentiment-pie-chart",
+                )]),
+        html.Br(),
        html.Div([
            dcc.Graph(id='compound-sentiment-line-graph', 
-                    figure = px.line(sentiment_means, x=sentiment_means['authoredAt'], y=['compound_7day'], labels={'compound_7day':'Average Compound Sentiment'}))]),
+                    figure = px.line(sentiment_means, x=sentiment_means['authoredAt'], y=['compound_7day'], labels={'compound_7day':'Average Compound Sentiment'},
+                                     title = 'Average Compound Sentiment Over Time <br><sup>Plot Subtitle</sup>'),
+                    style = {'border': '2px solid #e8e8e8', 'background-color': '#f8f9fa'}),
+                    html.P('The Compound Sentiment is a normalized measure of how positive or negative a post is classified to be. The closer to 1 this normalized value is, the more positive the post sentiment is. The closer to -1 the value is, the more negative the post sentiment is.')
+                    ]),
         html.Br(),
 
-        html.Div(dcc.Graph(id='posts-line-graph', 
-                    figure = px.line(posts_counts, x=posts_counts['authoredAt'], y=posts_counts['post_count'],
-                    title='Average Number of Posts Over Time', labels={'authoredAt':'Date', 'value':'Posts Count'}))),
+        html.Div(dcc.Graph(
+                    id='posts-line-graph', 
+                    figure = px.line(
+                        posts_counts, 
+                        x=posts_counts['authoredAt'], 
+                        y=posts_counts['post_count'],
+                        title='Average Number of Posts Over Time', 
+                        labels={'authoredAt':'Date', 'value':'Posts Count'}),
+                    style = {'border': '2px solid #e8e8e8', 'background-color': '#f8f9fa'})),
         html.Br(),
-
-        # html.Div(dcc.Graph(
-        #             id='sentiment-pie-chart',
-        #             figure=px.pie(
-        #                 sentiment_count,
-        #                 names='sentiment',
-        #                 values='count',
-        #                 title='Post Sentiment Breakdown',
-        #                 color_discrete_sequence=px.colors.sequential.RdBu,
-        #                 labels={'sentiment': 'Sentiment', 'count': 'Count'}
-        #             ),
-        #             style={'border': '2px solid #e8e8e8', 'background-color': '#f8f9fa'}
-        #         )),
-        # html.Br(),
     ],
     style=CONTENT_STYLE
 )
@@ -254,57 +268,12 @@ def date_options(visibility_state):
         return hide, show
     else:
         return show, hide
-    
 
-# # Add a callback function for `platform-selection` as input, `sentiment-pie-chart` as output
-# @app.callback(
-#         [Output("sentiment-pie-chart", "figure")],
-
-#         [Input("platform-selection", "value"),
-         
-#          # Account categories
-#          Input("all-or-none-category", "value"),
-#          Input("account-category", "value"),
-
-#          # Account identity
-#          Input("account-identity", "value"),
-
-#          # Account type
-#          Input("account-type", "value"),
-
-#          # Account location
-#          Input("account-location", "value"),
-
-#          # Time frame values
-#          Input("time-frame", "value"),
-#          Input("relative-date-div", "value"),
-#          Input("date-range", "start_date"),
-#          Input("date-range", "end_date")
-
-#          # Input("com-b-components", "value") - COM-B components for later implementation
-#          ])
-# def get_pie_chart(platform_list, select_all_none_category, account_category, account_identity, account_type, account_location, time_frame, relative_date, start_date, end_date):
-#     filtered_df = dataframe_filter(sm_df, platform_list, select_all_none_category, account_category, account_identity, account_type, account_location, time_frame, relative_date, start_date, end_date)
-
-#     sentiment_count = filtered_df.groupby(['sentiment'])['sentiment'].count().reset_index(name='count')
-#     figure = px.pie(
-#         sentiment_count,
-#         names='sentiment',
-#         values='count',
-#         title='Post Sentiment Breakdown',
-#         color_discrete_sequence=px.colors.sequential.RdBu)
-    
-#     figure.update_layout(
-#         title={'text': 'Post Sentiment Breakdown', 'x': 0.5, 'font': {'size': 20, 'color': 'black'}, 'y': 0.9},
-#         legend={'title': 'Sentiment', 'font': {'size': 14, 'color': 'black'}}
-#     )
-    
-#     return figure
-
-# Compounnd Sentiment Line Graph - Callback Function
+# Graph + Chart Callback Functions
 @app.callback(
         Output("compound-sentiment-line-graph", "figure"),
         Output("posts-line-graph", "figure"),
+        Output("sentiment-pie-chart", "figure"),
     
         # Platform Selection
         Input("platform-selection", "value"),
@@ -345,6 +314,7 @@ def line_graphs(platform_list, account_category, account_identity, account_type,
     compound_sentiment_line_graph.add_trace(go.Scatter(x=filtered_df['authoredAt'], y=filtered_df['compound_7day'], mode='lines', name='Compound (7-day)', line=dict(color='blue', width=2))) # Add the compound sentiment line with adjusted style
     compound_sentiment_line_graph.add_hline(y=0)
     compound_sentiment_line_graph.update_layout( # Update the layout of the figure
+        title = 'Average Compound Sentiment Over Time',
         xaxis_title='Date',
         yaxis_title='Sentiment',
         legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1),
@@ -354,7 +324,15 @@ def line_graphs(platform_list, account_category, account_identity, account_type,
         xaxis_range=[start_date, end_date],
         yaxis_range=[-1, 1],
         legend_title="Sentiment Lines",
-        showlegend=True
+        showlegend=True,
+        # annotations = [dict(
+        # x=0.5,
+        # y=-0.2,    #Trying a negative number makes the caption disappear - I'd like the caption to be below the map
+        # xref='paper',
+        # yref='paper',
+        # text='',
+        # showarrow = False
+        # )]
     )
 
     # Posts Line Graph
@@ -378,8 +356,26 @@ def line_graphs(platform_list, account_category, account_identity, account_type,
         legend_title="Posts"
     )
 
-    return compound_sentiment_line_graph, posts_line_graph
-    
+    # Sentiment Pie Chart
+    filtered_df['sentiment'] = filtered_df['compound'].apply(lambda x: 'positive' if x >= 0.1 else ('negative' if x <= -0.1 else 'neutral'))
+    sentiment_count = filtered_df.groupby(['sentiment'])['sentiment'].count().reset_index(name='count')
+    sentiment_pie_chart = px.pie(
+        sentiment_count,
+        names='sentiment',
+        values='count',
+        title='Post Sentiment Breakdown',
+        labels={'sentiment': 'Sentiment', 'count': 'Count'},
+        color='sentiment',
+        color_discrete_map={'positive': 'green', 'negative': 'red', 'neutral': 'blue'}
+    )
+    sentiment_pie_chart.update_layout(
+        font_family='Open Sans',
+        font_color='black',
+        title_x=0.5,
+        legend_title="Sentiment"
+    )
+
+    return compound_sentiment_line_graph, posts_line_graph, sentiment_pie_chart
 
 def dataframe_filter(sm_df, platform_list, account_category, account_identity, account_type, account_location, time_frame, relative_date, start_date, end_date):
     ## Filter the dataframe based on the selected platforms and the selected labels
@@ -434,6 +430,9 @@ def dataframe_filter(sm_df, platform_list, account_category, account_identity, a
             filtered_df = filtered_df[filtered_df['authoredAt'] >= start_date]
 
     else:
+        date_format = '%Y-%m-%d'
+        start_date = datetime.strptime(start_date, date_format).date() if isinstance(start_date, str) else start_date
+        end_date = datetime.strptime(end_date, date_format).date() if isinstance(end_date, str) else end_date
         filtered_df = filtered_df[(filtered_df['authoredAt'] >= start_date) & (filtered_df['authoredAt'] <= end_date)]
 
     return [filtered_df, start_date, end_date]
